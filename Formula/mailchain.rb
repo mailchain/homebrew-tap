7c7080d72022dd8a24e3e2467cc57b4d17a91924
goreleaserbot <goreleaser@carlosbecker.com>
2019-07-12 00:19:06 +0100

    Brew formula update for mailchain version v0.0.20

--- Formula/mailchain.rb
+++ Formula/mailchain.rb
@@ -2,9 +2,9 @@
 class Mailchain < Formula
   desc "Secure, email-like messaging for blockchain addresses and accounts"
   homepage "https://mailchain.xyz"
-  url "https://github.com/mailchain/mailchain/releases/download/v0.0.19/mailchain-v0.0.19-macOS-64bit.tar.gz"
-  version "0.0.19"
-  sha256 "ddc9863494652756520c0bfbf88b20fe85a1648483753cedbc20b37829c96bc2"
+  url "https://github.com/mailchain/mailchain/releases/download/v0.0.20/mailchain-v0.0.20-macOS-64bit.tar.gz"
+  version "0.0.20"
+  sha256 "b25caba05a1eebb2419ab1d90c30cef42c3a271192210b6d1515f08379d78599"
 
   def install
     bin.install "mailchain"
