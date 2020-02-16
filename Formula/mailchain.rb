fcdf833c2a84330f090b69d38bf5362f24d4f0e5
goreleaserbot <goreleaser@carlosbecker.com>
2020-02-16 19:13:29 +0000

    Brew formula update for mailchain version v0.0.45

--- Formula/mailchain.rb
+++ Formula/mailchain.rb
@@ -2,16 +2,16 @@
 class Mailchain < Formula
   desc "Secure, email-like messaging for blockchain addresses and accounts"
   homepage "https://mailchain.xyz"
-  version "0.0.44"
+  version "0.0.45"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/mailchain/mailchain/releases/download/v0.0.44/mailchain-v0.0.44-macOS-64bit.tar.gz"
-    sha256 "9b80b86d2d336107f08d5fbafc7a3192071dc20f7695d88ff9d3c4e7ae5f7e8e"
+    url "https://github.com/mailchain/mailchain/releases/download/v0.0.45/mailchain-v0.0.45-macOS-64bit.tar.gz"
+    sha256 "2f48fd8bba57b748400c00efb628855a6ce9a0d01697909880791317dd0daf3a"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/mailchain/mailchain/releases/download/v0.0.44/mailchain-v0.0.44-Linux-64bit.tar.gz"
-      sha256 "7e2c7193fe388de29e50828d88aa6d306a224fa07dbd2ede809d41fe9fb768d2"
+      url "https://github.com/mailchain/mailchain/releases/download/v0.0.45/mailchain-v0.0.45-Linux-64bit.tar.gz"
+      sha256 "db3c24b6ba6133524a001401fcfaa7ea6ebcbade568ac5000819992f4a5c8d55"
     end
   end
 
